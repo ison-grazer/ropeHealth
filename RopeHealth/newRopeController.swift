4e0df9acd90bcfa45fdfcb9b58460cd81375217f
Belle <fraansson@gmail.com>
2019-07-19 21:30:45 +0200

    first commit

---
+++ RopeHealth/newRopeController.swift
@@ -0,0 +1,30 @@
+//
+//  newRopeController.swift
+//  ropeHealth
+//
+//  Created by Isabella Fransson on 2019-07-17.
+//  Copyright © 2019 Isabella Fransson. All rights reserved.
+//
+
+import UIKit
+
+class newRopeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
