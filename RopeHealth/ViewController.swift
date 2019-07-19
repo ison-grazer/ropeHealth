38ac007db3df65e5dcc023a705deacdd7026005d
Belle <fraansson@gmail.com>
2019-07-19 21:37:54 +0200

    ?

---
+++ RopeHealth/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ropeHealth
+//
+//  Created by Isabella Fransson on 2019-07-11.
+//  Copyright © 2019 Isabella Fransson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var buttonContainer: UIImageView!
+    @IBOutlet weak var statsBtn: UIButton!
+    @IBOutlet weak var newRopeBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonContainer.layer.cornerRadius = 20 //rounds corners
+        // Do any additional setup after loading the view.
+    }
+  
+    
+    @IBAction func statsPressed(_ sender: Any) {
+    }
+    
+    @IBAction func newRopePressed(_ sender: Any) {
+    }
+
+}
+
--- RopeHealth/newRopeController.swift
+++ RopeHealth/newRopeController.swift
@@ -10,21 +10,79 @@ import UIKit
 
 class newRopeController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var buttonContainer: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterNameField: UITextField!
+    @IBOutlet weak var dateOfFstUse: UITextField!  //prompts date-picker
+    
+    
+    @IBOutlet weak var lenghtSlider: UISlider!
+    @IBOutlet weak var lenghtPrintBox: UITextField!
+    
+    @IBOutlet weak var widthSlider: UISlider!
+    @IBOutlet weak var widthPrintBox: UITextField!
+    @IBOutlet weak var doneBtn: UIButton!
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var footer: UIImageView!
+    
+    @IBAction func lengthSliderChanged(_ sender: UISlider) { //in m
+        let currentValue = Int(sender.value)
+        lenghtPrintBox.text = "\(currentValue)"
+    }
+    
+    @IBAction func widthSliderChanger(_ sender: UISlider) { //in mm
+        let currentValue = Double(sender.value)
+        let decValue = convertDecimals(num: currentValue)
+        widthPrintBox.text = "\(decValue)"
+    }
+    
+    func convertDecimals (num: Double) -> Double {
+        let x = Double(Int(num*10))/10
+        return x
+    }
+    
+    lazy var datePicker: UIDatePicker = {
+        
+        let picker = UIDatePicker() //instance of datePicker
+        
+        picker.datePickerMode = .date //not time
+        
+        picker.addTarget(self, action: #selector(datePickerChanged( _:)), for: .valueChanged) //will execute the method
+        
+        return picker
+    }()
+    
+    lazy var dateFormatter: DateFormatter = {
+       
+        let formatter = DateFormatter()
+        
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+            
+        return formatter //smthng is off
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonContainer.layer.cornerRadius = 20 //rounds corners
 
+        dateOfFstUse.inputView = datePicker //when interacting with bar
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func datePickerChanged(_ sender: UIDatePicker){   //creates a string from the data from method
+        dateOfFstUse.text = dateFormatter.string(from: sender.date)
     }
-    */
-
+   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){ //when tapping outside the field it closes
+      view.endEditing(true)
+    }
+    @IBAction func doneBtn(_ sender: UIButton) {
+        //will store data, show entry ok/edit then send to main page or "edit existing"
+    }
 }
+
+
+
